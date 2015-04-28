60cc122bfc5182635bf1196626add32ae47fa41c
Tom <mclaughlin.tho@gmail.com>
2015-04-28 01:15:01 +0000

    fixed migration

--- db/migrate/20150421015028_add_gallery_id_column_to_images.rb
+++
@@ -1,5 +0,0 @@
-class AddGalleryIdColumnToImages < ActiveRecord::Migration
-  def change
-    add_column :images, :gallery_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150421015028) do
+ActiveRecord::Schema.define(version: 20150421010132) do
 
   create_table "galleries", force: :cascade do |t|
     t.string   "name"
@@ -23,10 +23,12 @@ ActiveRecord::Schema.define(version: 20150421015028) do
   create_table "images", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
+    t.integer  "gallery_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "gallery_id"
   end
+
+  add_index "images", ["gallery_id"], name: "index_images_on_gallery_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
