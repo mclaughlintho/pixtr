a47cb5512d96c2fe535ae28c5917858cb86a799c
Tom <mclaughlin.tho@gmail.com>
2015-05-03 20:27:05 +0000

    user/destroy fixed and added header/footer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502060711) do
+ActiveRecord::Schema.define(version: 20150503192550) do
 
   create_table "galleries", force: :cascade do |t|
     t.string   "name"
@@ -38,9 +38,11 @@ ActiveRecord::Schema.define(version: 20150502060711) do
     t.integer  "gallery_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "images", ["gallery_id"], name: "index_images_on_gallery_id"
+  add_index "images", ["user_id"], name: "index_images_on_user_id"
 
   create_table "tags", force: :cascade do |t|
     t.string   "text"
